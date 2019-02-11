396a028afc4c3690a8878cf948d6a380cfe6d561
akihironemoto <akihiro.nemoto.06@gmail.com>
2019-02-12 00:13:25 +0900

    Logout

--- leadmanager/frontend/src/actions/auth.js
+++ leadmanager/frontend/src/actions/auth.js
@@ -3,7 +3,10 @@ import { returnErrors } from './messages';
 import {
   USER_LOADED,
   USER_LOADING,
-  AUTH_ERROR
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT_SUCCESS
 } from './types';
 
 // check token & load user
@@ -37,5 +40,62 @@ export const loadUser = () => (dispatch, getState) => {
       dispatch({
         type: AUTH_ERROR
       });
-    })
+    });
+}
+
+// login user
+export const login = (username, password) => dispatch => {
+
+  // headers
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+
+  // request body
+  const body = JSON.stringify({ username, password });
+
+  axios
+    .post('/api/auth/login', body, config)
+    .then(res => {
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data
+      });
+    }).catch(err => {
+      dispatch(returnErrors(err.response.data, err.response.status));
+      dispatch({
+        type: LOGIN_FAIL
+      });
+    });
+};
+
+// logout user
+export const logout = () => (dispatch, getState) => {
+
+  // get token from state
+  const token = getState().auth.token;
+
+  // headers
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+
+  // if token, add to headers config
+  if (token) {
+    config.headers['Authorization'] = `Token ${token}`;
+  }
+
+  axios
+    .post('/api/auth/logout/', null, config)
+    .then(res => {
+      dispatch({
+        type: LOGOUT_SUCCESS
+      });
+    }).catch(err => {
+      dispatch(returnErrors(err.response.data, err.response.status));
+    });
 }
